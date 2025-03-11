b31b2545df689ca41dd10261d70e1262b6fb2355
GiantFace <modroczkyferenc0221@gmail.com>
2025-03-11 15:45:35 +0100

    Hyperspeed added, (for fun)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-
+import Hyperspeed from "@/components/ui/Hyperspeed";
 
 const Home = () => {
   const router = useRouter();
@@ -12,10 +12,16 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold mb-4">Press to login!</h1>
-      <Button onClick={handleLogin}>Login</Button>
-      <p className="italic mt-3.5">(just for presenting)</p>
+    <div className="flex flex-col">
+      <div className="justify-items-center p-0 gap-0 ">
+        <h1 className="text-3xl font-bold">Press to login!</h1>
+        <Button onClick={handleLogin}>Login</Button>
+        <p className="italic">(just for presenting)</p>
+      </div>
+
+      <div className="flex flex-col items-center overflow-x-scroll-hidden">
+        <Hyperspeed />
+      </div>
     </div>
   );
 };
